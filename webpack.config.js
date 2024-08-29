303d8bea64fcb4539529ec0a0a8ecd1e9c7dd3ae
Phu <33654804+lequangphu@users.noreply.github.com>
2024-08-29 11:09:30 +0700

    handle bundled index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,4 +10,13 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
